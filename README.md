a1350b78dfd928679ff6e0a0f9863af9e0b54364
grptx <robertorossetti3.14@gmail.com>
2017-07-21 15:27:40 +0200

    some methods added

--- README.md
+++ README.md
@@ -10,8 +10,26 @@ This component wraps [kreait/firebase-php](https://github.com/kreait/firebase-ph
     'firebase' => [
         'class'=>'grptx\Firebase\Firebase',
         'credential_file'=>'service_account.json', // (see https://firebase.google.com/docs/admin/setup#add_firebase_to_your_app)
-        'database_uri'=>'https://my-project.firebaseio.com',
+        'database_uri'=>'https://my-project.firebaseio.com', // (optional)
     ]
 ...
 ]
 ```
+
+## Usage
+
+Retrive a _reference_ and a _value_
+```php
+$database = Yii::$app->firebase->getDatabase();
+$reference = $database->getReference('path/to/child/location');
+$value = $reference->getValue();
+```
+
+or just the _value_
+
+```php
+$reference = Yii::$app->firebase->getReference('path/to/child/location');
+$value = $reference->getValue();
+```
+
+for other method see [firebase-php.readthedocs.io.](https://firebase-php.readthedocs.io/en/latest/realtime-database.html)
--- src/Firebase.php
+++ src/Firebase.php
@@ -9,12 +9,14 @@
 namespace grptx\Firebase;
 
 use Kreait\Firebase\Database;
+use Kreait\Firebase\Database\Reference;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 use Kreait\Firebase\Exception\ServiceAccountDiscoveryFailed;
 use yii\base\Component;
 
-class Firebase extends Component {
+class Firebase extends Component
+{
     /**
      * @var string
      */
@@ -22,7 +24,7 @@ class Firebase extends Component {
     /**
      * @var string
      */
-    public $database_uri;
+    public $database_uri = false;
     /**
      * @var ServiceAccount
      */
@@ -39,17 +41,17 @@ class Firebase extends Component {
     /**
      * @return ServiceAccount
      */
-    public function getServiceAccount()
+    public function getServiceAccount(): ServiceAccount
     {
-        if(!$this->_serviceAccount) {
-            try{
-                if(!$this->credential_file) {
+        if (!$this->_serviceAccount) {
+            try {
+                if (!$this->credential_file) {
                     $this->_serviceAccount = ServiceAccount::discover();
                 } else {
                     $this->_serviceAccount = ServiceAccount::fromJsonFile($this->credential_file);
                 }
             } catch (ServiceAccountDiscoveryFailed $e) {
-                $this->_serviceAccount=null;
+                $this->_serviceAccount = null;
             }
 
         }
@@ -59,7 +61,7 @@ class Firebase extends Component {
     /**
      * @return string
      */
-    public function getDataBaseUri()
+    public function getDataBaseUri(): string
     {
         return $this->database_uri;
     }
@@ -67,17 +69,17 @@ class Firebase extends Component {
     /**
      * @return \Kreait\Firebase
      */
-    public function getFirebase()
+    public function getFirebase(): \Kreait\Firebase
     {
-        if(!$this->_firebase){
-            if($this->getServiceAccount()) {
+        if (!$this->_firebase) {
+            if ($this->getServiceAccount()) {
                 $factory = (new Factory)->withServiceAccount($this->getServiceAccount());
-                if($this->getDataBaseUri()) {
+                if ($this->getDataBaseUri()) {
                     $this->_firebase = $factory->withDatabaseUri($this->getDataBaseUri())->create();
                 } else {
                     $this->_firebase = (new Factory)->withServiceAccount($this->getServiceAccount())->create();
                 }
-            } else if($this->getDataBaseUri()) {
+            } else if ($this->getDataBaseUri()) {
                 $this->_firebase = (new Factory)->withDatabaseUri($this->getDataBaseUri())->create();
             } else {
                 throw new ServiceAccountDiscoveryFailed("cannot retrieve Firebase istance");
@@ -89,15 +91,32 @@ class Firebase extends Component {
     /**
      * @return Database
      */
-    public function getDatabase()
+    public function getDatabase(): Database
     {
-        if(!$this->_database) {
-            if($this->getFirebase()) {
+        if (!$this->_database) {
+            if ($this->getFirebase()) {
                 $this->_database = $this->getFirebase()->getDatabase();
             }
         }
         return $this->_database;
     }
 
+    /**
+     * @param string $path
+     * @return Reference
+     */
+    public function getReference(string $path = ''): Reference
+    {
+        return $this->getDatabase()->getReference($path);
+    }
+
+    /**
+     * @param $uri
+     * @return Reference
+     */
+    public function getReferenceFromUrl($uri): Reference
+    {
+        return $this->getDatabase()->getReferenceFromUrl($uri);
+    }
 
 }
